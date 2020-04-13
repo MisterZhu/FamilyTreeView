507374c831c86364492b8a2e69e4363d66819828
lixiao <1280253718@qq.com.com>
2020-04-13 08:34:38 +0800

    first commit

---
+++ FamilyTreeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FamilyTreeView
+//
+//  Created by ZupuMac on 2020/4/10.
+//  Copyright © 2020 ZLX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target 'FamilyTreeView' do
+
+pod 'RealmSwift'
+
+end
